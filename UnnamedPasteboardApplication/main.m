a6830eda60a2080a25264080577956976f8fd20e
thomascole <thmcole@gmail.com>
2013-07-27 15:25:33 -0400

    Initial Commit

---
+++ UnnamedPasteboardApplication/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  UnnamedPasteboardApplication
+//
+//  Created by Thomas Cole on 7/27/13.
+//  Copyright (c) 2013 co1e. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
---
+++ UnnamedPasteboardApplicationTests/UnnamedPasteboardApplicationTests.m
@@ -0,0 +1,36 @@
+//
+//  UnnamedPasteboardApplicationTests.m
+//  UnnamedPasteboardApplicationTests
+//
+//  Created by Thomas Cole on 7/27/13.
+//  Copyright (c) 2013 co1e. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface UnnamedPasteboardApplicationTests : XCTestCase
+
+@end
+
+@implementation UnnamedPasteboardApplicationTests
+
+- (void)setUp
+{
+    [super setUp];
+    
+    // Set-up code here.
+}
+
+- (void)tearDown
+{
+    // Tear-down code here.
+    
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    XCTFail(@"No implementation for \"%s\"", __PRETTY_FUNCTION__);
+}
+
+@end
